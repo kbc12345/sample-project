308ebd92941283281314e84125fe72eb8de49b81
rav bay <ravzski@gmail.com>
2015-05-27 17:18:00 +0800

    added careers page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root :to => "site/home#index"
   get 'contacts', to: "site/home#contacts"
+  get 'careers', to: "site/home#careers"
+  get 'works', to: "site/home#works"
+  get 'about', to: "site/home#about"
+  get 'blog', to: "site/home#blog"
+
 
   get 'cms' => 'cms#index'
   get 'cms/*path' => 'cms#index'
